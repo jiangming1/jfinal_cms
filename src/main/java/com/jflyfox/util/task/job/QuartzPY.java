2f23f4d224790536cd616e8819ccdbeff58cbbf7
[qiukj] <[175683435@qq.com]>
2018-05-18 15:32:06 +0800

    jboss

--- src/main/java/com/jflyfox/util/task/job/QuartzPY.java
+++ src/main/java/com/jflyfox/util/task/job/QuartzPY.java
@@ -6,8 +6,8 @@ public class QuartzPY {
 
     public void runPYtrigger() {
             try {
-                System.out.println("【定时任务启动】开始()...");
-                QuartzManager.addJob("py", SpiderJob.class, "*/5 * * * * ?","2");
+//                System.out.println("【定时任务启动】开始()...");
+//                QuartzManager.addJob("py", SpiderJob.class, "*/5 * * * * ?","2");
             } catch (Exception e) {
                 e.printStackTrace();
             }
